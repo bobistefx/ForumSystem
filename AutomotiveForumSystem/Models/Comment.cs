001fd9dd139f43883dd8f301097f964bfbfeab61
ventsy <venci1983@gmail.com>
2023-12-15 16:21:34 +0200

    add create date property to Comment model

--- AutomotiveForumSystem/Models/Comment.cs
+++ AutomotiveForumSystem/Models/Comment.cs
@@ -12,6 +12,9 @@ namespace AutomotiveForumSystem.Models
         [Required, MinLength(32), MaxLength(8192)]
         public string Content { get; set; } = string.Empty;
 
+        [Required]
+        public DateTime CreateDate { get; set; }
+
         public IList<Comment> Replies { get; set; } = new List<Comment>();
 
         [Required]
