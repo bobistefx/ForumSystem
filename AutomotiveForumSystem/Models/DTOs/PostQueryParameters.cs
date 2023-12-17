7457f698b351df5b7dd661c151ea130fcda7fedb
Borislav Stefanov <traev50@gmail.com>
2023-12-17 17:08:15 +0200

    made postqueryparams nullable

---
+++ AutomotiveForumSystem/Models/DTOs/PostQueryParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace AutomotiveForumSystem.Models.PostDtos
+{
+    public class PostQueryParameters
+    {
+        public string? Category { get; set; }
+        public string? Title { get; set; }
+    }
+}
